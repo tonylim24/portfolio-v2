3dac3d5f13aa46ee85e026273a65286522862668
Tony Salim <tony@pastryclass.com>
2021-11-13 20:17:52 -0800

    Header component.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <h3>Footer Here</h3>
+    </div>
+  );
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import Landing from "./views/Landing/Landing";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Landing />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
